204d2d98643a61fc0fa9707749b02a5a2efb7025
Abrar051 <shahriar1410051@gmail.com>
2020-12-02 15:17:41 +0600

    first commit

---
+++ src/logic_learn.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class logic_learn {
+    public static void main (String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        int number2;
+        int count=1,count2=1;
+        //number2=number/10;
+        while (true)
+        {
+            count++;
+            number2=number/1;
+            System.out.println (count);
+            if (number2<1)
+            {
+                break;
+            }
+        }
+    }
+}
